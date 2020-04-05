c76bf043b5084dd9087fe41d6df7752ba5f09f19
Nicholas Keller <nic.keller328@gmail.com>
2020-04-05 23:53:39 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,93 @@
+class TicTacToe
+  
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ]
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input)
+    input.to_i - 1
+  end
+  
+  def move(location, token="X")
+    square = location.to_i
+    @index = square
+    @board[@index] = token
+  end
+  
+  def position_taken?(index)
+    position = index.to_i
+    if @board[position] == "X" || @board[position] == "O"
+      true
+    else
+      false
+    end
+  end 
+    
+  def valid_move?(input)
+    num = input.to_i
+    if num >= 0 && num <= 9
+      valid_number = num
+    else
+      false
+    end
+    if @board[valid_number] == "X" || @board[valid_number] == "O"
+      false
+    else
+      true
+    end
+  end
+    
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
+  
+  def turn_count
+  end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+end
